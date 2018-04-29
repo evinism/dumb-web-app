ac908fbcd6f5501b9d3569b667895e2018b0404e
Ray Shih <rayshih@fb.com>
2018-04-29 10:35:20 -0700

    try in memory state

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,31 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Control.Applicative
-import           Snap.Core
-import           Snap.Util.FileServe
-import           Snap.Http.Server
+import Data.IORef
+import Control.Monad.State.Lazy
+import Control.Lens.TH
+import Snap.Core
+import Snap.Http.Server
+import Snap
+import Data.Text
 
+data App = App { _counter :: IORef Int }
+
+makeLenses ''App
+
+appInit :: SnapletInit App App
+appInit = makeSnaplet "myapp" "My example application" Nothing $ do
+  addRoutes [("count", countHandler)]
+  ref <- liftIO $ newIORef 0
+  return $ App ref
+
+countHandler :: Handler App App ()
+countHandler = method GET $ do
+  ref <- gets _counter
+  liftIO $ modifyIORef ref (+ 1)
+  c <- liftIO $ readIORef ref
+  writeText $ pack $ show c
+  
 main :: IO ()
-main = quickHttpServe site
-
-site :: Snap ()
-site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
-
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+main = serveSnaplet defaultConfig appInit
